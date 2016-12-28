5af125c8ef2d8841d956d4a23ac153b6e9338946
Prawal sharma <prawalsharma@prawals-mbp.attlocal.net>
2016-12-27 18:47:21 -0600

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Prawal sharma on 12/27/16.
+//  Copyright © 2016 NunyaBusiness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
